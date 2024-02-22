3892deadb6a25672df4bb802290737beb865e14e
Opal Leung <opalleunglch@mailbox.org>
2024-02-22 18:24:08 +0100

    use game service in useGames hook

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,15 +1,11 @@
 import React, {useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
+import gameService from '../services/game-service'
 import { CanceledError } from 'axios';
 
-interface Game{
+export interface Game{
     id: number;
     name: string;
-}
-
-interface FetchGamesResponse{
-    count: number;
-    results: Game[];
+    background_image: string; // url
 }
 
 const useGames = () => {
@@ -19,15 +15,17 @@ const useGames = () => {
     useEffect(
         () => {
         const controller = new AbortController();
-        apiClient.get<FetchGamesResponse>('/games')
-            .then(res => setGames(res.data.results)) // success 
-            .catch((err) => {
-                // CanceledError is raised when controller.abort() is called
-                if (err instanceof CanceledError) return; 
-                setError(err.message);
+        const {request, cancel} =  gameService.getAll();
+
+            request
+                .then(res => setGames(res.data.results)) // success 
+                .catch((err) => {
+                    // CanceledError is raised when controller.abort() is called
+                    if (err instanceof CanceledError) return; 
+                    setError(err.message);
             })        
         // clean-up: cancel all fetch request before unmounting
-        return () => controller.abort(); 
+        return cancel;
         }, [] // request will be sent constantly without deps
     )
     return {games, error}
