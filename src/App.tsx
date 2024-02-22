a61c592a88d00e9b3524c05feaed35623c10daeb
Opal Leung <opalleunglch@mailbox.org>
2024-02-22 10:00:47 +0100

    add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
-import reactLogo from './assets/react.svg'
+import NavBar from './components/NavBar'
 import './App.css'
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <GridItem area={'main'} bg={'black'}>Main</GridItem>
       </Show>
       <Show above='base'>
-        <GridItem area={'nav'} bg={'coral'}>Nav</GridItem>
+        <GridItem area={'nav'}><NavBar/></GridItem>
         <GridItem area={'main'} bg={'black'}>Main</GridItem>
       </Show>
     </Grid>
