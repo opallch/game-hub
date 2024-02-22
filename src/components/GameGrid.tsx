f40310753df6d8e4af16d48442b8b540d4362fef
Opal Leung <opalleunglch@mailbox.org>
2024-02-22 23:11:04 +0100

    add platform icon list to GameCard

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,5 +13,5 @@ const GameGrid = () => {
         </SimpleGrid>
         )
 }
- 
+
 export default GameGrid;
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,10 +2,17 @@ import React, {useEffect, useState } from 'react';
 import gameService from '../services/game-service'
 import { CanceledError } from 'axios';
 
+export interface Platform{
+    id: number,
+    name: string,
+    slug: string
+}
+
 export interface Game{
     id: number;
     name: string;
     background_image: string; // url
+    parent_platforms: {platform: Platform}[];
 }
 
 const useGames = () => {
